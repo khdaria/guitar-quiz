5ba3b400981166ff9654ce7ec68aab6ffb69e40d
Daria Kharkina <daria.kharkina@Darias-MacBook-Pro.local>
2025-04-23 14:17:29 +0300

    working sheets script

--- src/Quiz.js
+++ src/Quiz.js
@@ -163,6 +163,22 @@ const Quiz = () => {
   const mbtiResult = getMBTI();
   const resultData = typeDescriptions[mbtiResult];
 
+  // 👇 SEND TO GOOGLE SHEETS HERE
+  fetch('https://script.google.com/macros/s/AKfycbxKiM7PVxnPemaMNj1st5atjAbdt5qBSu41A1YsYNY-cAFIo-JcccfdmMz4YfKFikvHoQ/exec', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      ei: answers.ei,
+      sn: answers.sn,
+      tf: answers.tf,
+      jp: answers.jp,
+      resultTitle: resultData.title
+    })
+  });
+  
+
   return (
     <div className="container">
       {current === -1 ? (
